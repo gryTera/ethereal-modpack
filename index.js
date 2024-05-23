4d7daad397ed19cacb87b3255c5bcbaf2c27bdb1
gryTera <bruce+gry@myinbox.ws>
2024-05-23 08:57:45 -0400

    Update debug logging

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const { autoRestartGuildQuests } = require('./src/quest-completer');
 
 class EtherealModpack {
 	constructor(mod) {
-		console.log(JSON.stringify(mod.clientInterface.info.protocol, null, 2));
+		mod.log('protocol map version: ' + mod.clientInterface.info.protocolVersion);
+		mod.log(JSON.stringify(mod.clientInterface.info.protocol, null, 2));
 
 		new Util(mod).then(utils => {
 			autoRestartGuildQuests(utils);
