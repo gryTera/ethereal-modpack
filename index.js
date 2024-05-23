accfdda8289e02b147e28259f20a99803166bf06
gryTera <bruce+gry@myinbox.ws>
2024-05-23 10:42:28 -0400

    Attempt adding opcodes

--- index.js
+++ index.js
@@ -4,21 +4,15 @@ const addNeededOpcodes = require('./src/opcodes');
 const { autoRestartGuildQuests } = require('./src/quest-completer');
 
 class EtherealModpack {
-	constructor(mod) {
-		// addNeededOpcodes(mod);
+  constructor(mod) {
+    addNeededOpcodes(mod);
 
-		mod.log(typeof mod.dispatch.protocolMap);
-		mod.log(JSON.stringify(mod.dispatch.protocolMap, null, 2));
-
-		// mod.log('protocol map version: ' + mod.clientInterface.info.protocolVersion);
-		// mod.log(JSON.stringify(mod.clientInterface.info.protocol, null, 2));
-
-		new Util(mod).then(utils => {
-			autoRestartGuildQuests(utils);
-		}).catch(e => {
-			mod.error(e);
-		});
-	}
+    new Util(mod).then(utils => {
+      autoRestartGuildQuests(utils);
+    }).catch(e => {
+      mod.error(e);
+    });
+  }
 }
 
 module.exports = EtherealModpack;
--- src/opcodes.js
+++ src/opcodes.js
@@ -1,10 +1,17 @@
 function addNeededOpcodes(mod) {
-  const opCodeOverrides = {
+  // mod.log(typeof mod.dispatch.protocolMap);
+  // mod.log(JSON.stringify(mod.dispatch.protocolMap, null, 2));
+
+  // mod.log('protocol map version: ' + mod.clientInterface.info.protocolVersion);
+  // mod.log(JSON.stringify(mod.clientInterface.info.protocol, null, 2));
+
+  const opcodeOverrides = {
     C_REQUEST_FINISH_GUILD_QUEST: 41778,
   };
 
-  for (const [name, opcode] of Object.values(opCodeOverrides)) {
+  for (const [name, opcode] of Object.entries(opcodeOverrides)) {
     mod.clientInterface.info.protocol[name] = opcode;
+    mod.dispatch.addOpcode(name, opcode);
   }
 }
 
