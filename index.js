e7f4c438070469b5a18b79cc6e940d982bf366ba
gryTera <bruce+gry@myinbox.ws>
2024-05-23 08:36:43 -0400

    Add opcode debug

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { autoRestartGuildQuests } = require('./src/quest-completer');
 
 class EtherealModpack {
 	constructor(mod) {
+		console.log(JSON.stringify(mod.clientInterface.info.protocol, null, 2));
+
 		new Util(mod).then(utils => {
 			autoRestartGuildQuests(utils);
 		}).catch(e => {
