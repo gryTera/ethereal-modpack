338b21a78bdcc65210c967bcaa8cb996528cd253
gryTera <bruce+gry@myinbox.ws>
2024-05-31 13:35:47 -0400

    Additional debugging, update def for spawning servant

--- src/defs.js
+++ src/defs.js
@@ -2,12 +2,11 @@ const path = require('path');
 
 // Some defs are not available by default in tera toolbox. We load them here.
 function addDefs(mod) {
-  mod.dispatch.addDefinition('C_REQUEST_SPAWN_SERVANT', 1, path.join(__dirname, '..', 'defs', 'C_REQUEST_SPAWN_SERVANT.1.def'));
-	mod.dispatch.addDefinition('C_REQUEST_SPAWN_SERVANT', 2, path.join(__dirname, '..', 'defs', 'C_REQUEST_SPAWN_SERVANT.2.def'));
+  mod.dispatch.addDefinition('C_REQUEST_SPAWN_SERVANT', 4, path.join(__dirname, '..', 'defs', 'C_REQUEST_SPAWN_SERVANT.4.def'));
 	mod.dispatch.addDefinition('C_START_SERVANT_ACTIVE_SKILL', 1, path.join(__dirname, '..', 'defs', 'C_START_SERVANT_ACTIVE_SKILL.1.def'));
 	mod.dispatch.addDefinition('C_START_SERVANT_ACTIVE_SKILL', 2, path.join(__dirname, '..', 'defs', 'C_START_SERVANT_ACTIVE_SKILL.2.def'));
 	mod.dispatch.addDefinition('S_START_COOLTIME_SERVANT_SKILL', 1, path.join(__dirname, '..', 'defs', 'S_START_COOLTIME_SERVANT_SKILL.1.def'));
-	mod.dispatch.addDefinition('S_UPDATE_SERVANT_INFO', 1, path.join(__dirname, 'defs', '..', 'S_UPDATE_SERVANT_INFO.1.def'));
+	mod.dispatch.addDefinition('S_UPDATE_SERVANT_INFO', 1, path.join(__dirname, '..', 'defs', 'S_UPDATE_SERVANT_INFO.1.def'));
 }
 
 module.exports = addDefs;
